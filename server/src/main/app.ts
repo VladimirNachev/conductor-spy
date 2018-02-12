a17fbfef2ce08ac79693d136f0f9336793a891a2
Vladimir Nachev <vladimir.jivkov@gmail.com>
2018-02-12 18:56:05 +0200

    Create Routes endpoint

--- server/src/main/app.ts
+++ server/src/main/app.ts
@@ -2,6 +2,7 @@ import * as bodyParser from "body-parser";
 import * as debug from "debug";
 import * as express from "express";
 import stations from "./controllers/stations";
+import routes from "./controllers/routes";
 import { Log } from "./Log";
 
 Log.SERVER("Initializing...");
@@ -9,6 +10,7 @@ Log.SERVER("Initializing...");
 const app: express.Express = express();
 app.use(bodyParser.json());
 
-app.use("/", stations);
+app.use("/stations", stations);
+app.use("/routes", routes);
 
 export default app;
